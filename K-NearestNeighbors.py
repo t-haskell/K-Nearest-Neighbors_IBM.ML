515e921b804c522903b10f6f8da07183946b001c
Tommy <tommyhaskell3@gmail.com>
2023-12-27 17:50:31 -0500

    Docstring and importing packages

---
+++ K-NearestNeighbors.py
@@ -0,0 +1,14 @@
+'''
+Lab by -> Thomas Haskell
+
+Topic -> Classification with K-Nearest Neighbors
+Source -> IBM Machine Learning with Python Certification Course
+
+Objectives:
+1. Use K-Nearest Neighbors to classify a customer data set
+2. Learn how create, train, and test this type of model
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import preprocessing
